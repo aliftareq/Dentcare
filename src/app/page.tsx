a8eda55e635f25a624a8e209ee8488b05028b1a2
aliftareq <aliftareq@gmail.com>
2026-01-30 12:31:32 +0600

    cleark,neon,vapi ai assitent everything for auth,database and for voice call setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button>Click Me.</Button>
+    <div>
+      <h1>Home page</h1>
+    </div>
   );
 }
